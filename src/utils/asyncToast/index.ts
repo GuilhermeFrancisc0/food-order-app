4c4fc14e8c8154b5fbabffc36348ee03d405b2c9
Guilherme Francisco <guilhermefranciscoav@gmail.com>
2024-07-15 01:49:14 -0300

    (fix): #FOOD-19 Ajuste no componente de carousel, utilitário de toasts assincronos e no card das categorias

--- src/utils/asyncToast/index.ts
+++ src/utils/asyncToast/index.ts
@@ -3,12 +3,12 @@ import { toast } from 'react-toastify';
 import { getToastUpdateOptions } from '@/providers/ToastProvider';
 
 export const asyncToast = async (asyncFn: () => Promise<any>, loadingMsg: string, successMsg: string) => {
-  const id = toast.loading(loadingMsg);
+  const id = loadingMsg ? toast.loading(loadingMsg) : '';
 
   try {
     await asyncFn();
 
-    toast.update(id, getToastUpdateOptions(successMsg, 'success'));
+    successMsg && toast.update(id, getToastUpdateOptions(successMsg, 'success'));
   } catch (e: any | Error) {
     toast.update(id, getToastUpdateOptions(e.message, 'error'));
     throw e;
